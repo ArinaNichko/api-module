3e81556977efcc76c5e62f55424ce51d757e0571
ArinaNichko <104902526+ArinaNichko@users.noreply.github.com>
2023-04-26 13:09:24 +0300

    Some changes

--- src/test/java/enums/ResponseCodes.java
+++ src/test/java/enums/ResponseCodes.java
@@ -1,12 +1,7 @@
 package enums;
 
 public enum ResponseCodes {
-  OK(200),
-  CREATED(201),
-  BAD_REQUEST(400),
-  UNAUTHORIZED(401),
-  INTERNAL_SERVER_ERROR(500),
-  NOT_FOUND(404);
+  OK(200);
 
   private final int statusCode;
 
